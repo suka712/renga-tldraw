239ab8e0afcc2a08b1820b036a12f59a2560d0fa
khiem <khiemnguyen.hye@gmail.com>
2026-02-18 21:13:04 +0700

    Wire up email submit and OTP layout

--- client/pages/Login.tsx
+++ client/pages/Login.tsx
@@ -1,9 +1,41 @@
-import { Card, CardContent, CardFooter, CardHeader, CardDescription, CardTitle, CardAction } from '@/components/ui/card'
+import { Card, CardContent, CardFooter, CardHeader, CardDescription, CardTitle, } from '@/components/ui/card'
 import { Button } from '@/components/ui/button'
 import { Label } from '@/components/ui/label'
 import { Input } from '@/components/ui/input'
+import { InputOTP, InputOTPGroup, InputOTPSeparator, InputOTPSlot } from '@/components/ui/input-otp'
+import { useEffect, useState } from 'react'
+import { LucideEdit2, RefreshCcw } from 'lucide-react'
+
+type Toggle = 'login' | 'otp' | 'request'
 
 export const Portal = () => {
+  const [toggle, setToggle] = useState<Toggle>('login')
+  const [email, setEmail] = useState('')
+  const [otp, setOtp] = useState('')
+
+  useEffect(() => {
+    setToggle('otp')
+  }, [])
+
+
+  const handleSubmitEmail: React.MouseEventHandler<HTMLButtonElement> = async (e) => {
+    e.preventDefault()
+    try {
+      await fetch(`/api/auth/email`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          email: email
+        })
+      })
+      setToggle('otp')
+    } catch (e) {
+      console.log('Shit went wrong:', e)
+    }
+  }
+
   return (
     <div className='flex justify-center items-center h-screen'>
       <Card className="w-full max-w-sm py-10" >
@@ -15,22 +47,55 @@ export const Portal = () => {
           </CardDescription>
         </CardHeader>
         <CardContent>
+
           <form>
             <div className="flex flex-col gap-6">
-              <div className="grid gap-2">
-                <Label htmlFor="email">Email</Label>
-                <Input
-                  id="email"
-                  type="email"
-                  placeholder="poophead@sukaseven.com"
-                  required
-                />
+              <div className="">
+                {toggle === 'login' &&
+                  <>
+                    <Label htmlFor="email">Email</Label>
+                    <Input
+                      className='mt-2'
+                      id="email"
+                      type="email"
+                      value={email}
+                      onChange={e => setEmail(e.target.value)}
+                      placeholder="poophead@sukaseven.com"
+                      required
+                    />
+                  </>
+                }
+
+                {toggle === 'otp' &&
+                  <>
+                    <Label htmlFor="email">{`OTP sent to ${email}`}</Label>
+                    <div className='flex justify-between mt-2'>
+                      <InputOTP
+                        maxLength={6}
+                        value={otp}
+                        onChange={v => setOtp(v)}
+                      >
+                        <InputOTPGroup>
+                          <InputOTPSlot index={0} />
+                          <InputOTPSlot index={1} />
+                          <InputOTPSlot index={2} />
+                          <InputOTPSlot index={3} />
+                          <InputOTPSlot index={4} />
+                          <InputOTPSlot index={5} />
+                        </InputOTPGroup>
+                      </InputOTP>
+                      <Button variant={'outline'}><LucideEdit2 /></Button>
+                      <Button variant={'outline'}><RefreshCcw /></Button>
+                    </div>
+                  </>
+                }
               </div>
             </div>
           </form>
+
         </CardContent>
         <CardFooter className="flex-col gap-2">
-          <Button type="submit" className="w-full">
+          <Button type="submit" onClick={handleSubmitEmail} className="w-full">
             Login
           </Button>
           <Button variant="outline" className="w-full">
